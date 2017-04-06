e92dbf44b1aa4e904a7131002f00391706d4d88f
Jeremy Stretch <jstretch@digitalocean.com>
2017-04-06 11:54:13 -0400

    Fixes #1051: Upgraded django-rest-swagger
    Conflicts:
    	netbox/netbox/urls.py
    	requirements.txt

--- netbox/netbox/urls.py
+++ netbox/netbox/urls.py
@@ -1,3 +1,5 @@
+from rest_framework_swagger.views import get_swagger_view
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -8,6 +10,7 @@ from users.views import login, logout
 
 
 handler500 = handle_500
+swagger_view = get_swagger_view(title='NetBox API')
 
 _patterns = [
 
@@ -36,10 +39,15 @@ _patterns = [
     url(r'^api/ipam/', include('ipam.api.urls', namespace='ipam-api')),
     url(r'^api/secrets/', include('secrets.api.urls', namespace='secrets-api')),
     url(r'^api/tenancy/', include('tenancy.api.urls', namespace='tenancy-api')),
+<<<<<<< HEAD
     url(r'^api/docs/', include('rest_framework_swagger.urls')),
 
     # Serving static media in Django to pipe it through LoginRequiredMiddleware
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+=======
+    url(r'^api/docs/', swagger_view, name='api_docs'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+>>>>>>> f3eee25... Fixes #1051: Upgraded django-rest-swagger
 
     # Error testing
     url(r'^500/$', trigger_500),
